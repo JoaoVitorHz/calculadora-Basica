9c8659489da8970859c0094cf3a16cd0e98fc65b
João Vitor <vitorjoao39207@gmail.com>
2022-05-26 02:09:30 -0300

    Colocando placeholder nos inputs

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <form method="GET" action="calc.php">
 
-	<input type="number" name="n1" />
+	<input type="number" name="n1" placeholder="Primeiro numero" />
 
 	<select name="op">
 		<option>-</option>
@@ -9,7 +9,7 @@
 		<option>/</option>
 	</select>
 
-	<input type="number" name="n2" />
+	<input type="number" name="n2"  placeholder="Segundo numero" />
 
-	<input type="submit" value="Calcular" />
+	<input type="submit" value="Calcular"  />
 </form>
